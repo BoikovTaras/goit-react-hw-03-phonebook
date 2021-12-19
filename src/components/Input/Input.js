2f2800ca18e1095d1abdf21c95cca0d30dd1cc9e
Taras Boikov <tarasgowest@gmail.com>
2021-12-19 22:55:12 +0200

    Update Input.js

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -38,6 +38,7 @@ class Input extends Component {
   };
 
   render() {
+    const { name, number } = this.state;
     return (
       <form className={s.form}>
         <h3 className={s.title}>Name</h3>
@@ -46,7 +47,7 @@ class Input extends Component {
             className={s.input}
             type="text"
             name="name"
-            value={this.state.name}
+            value={name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -58,7 +59,7 @@ class Input extends Component {
             className={s.input}
             type="tel"
             name="number"
-            value={this.state.number}
+            value={number}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
